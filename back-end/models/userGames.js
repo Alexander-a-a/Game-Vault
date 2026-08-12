b15a852e85d3a575248c6f6c8f6886044ac6ad06
Alexander-a-a <alexander.antonsen.arno@hotmail.com>
2026-08-12 15:02:15 +0200

    finished userGames model

--- back-end/models/userGames.js
+++ back-end/models/userGames.js
@@ -6,6 +6,28 @@ module.exports = (sequlize, Sequelize) => {
       primaryKey: true,
       allowNull: false,
     },
+    userId: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+    },
+    gameId: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+    },
+    status: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+    },
+    userRating: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: true,
+    },
+    favorite: {
+        type: Sequelize.DataTypes.BOOLEAN,
+        defualtValue: false
+    }
+  }, {
+    timestamps: false,
   });
   UserGames.associate = function(models) {
     UserGames.belongsTo(models.User, { foreignKey: "userId" });

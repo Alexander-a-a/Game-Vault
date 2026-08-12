5124ae84ffc9224cb8c4fd227231a008498ecb0a
Alexander-a-a <alexander.antonsen.arno@hotmail.com>
2026-08-12 13:44:27 +0200

    finished games model and users

---
+++ back-end/models/users.js
@@ -0,0 +1,37 @@
+models.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: Sequelize.DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    firstname: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+    },
+    lastname: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+    },
+    username: {
+        type: Sequelize.DataTypes.STRING,
+        uniqe: true,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.DataTypes.STRING,
+        uniqe: true,
+        allowNull: false,
+    },
+    password: {
+        type: Sequelize.DataTypes.STRING,
+        uniqe: true,
+        allowNull: false,
+    },
+    telphone: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+    }
+  });
+};

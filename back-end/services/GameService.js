5b9c11c285b8fe9c5c400c9a3909746e3bac96c3
Alex <alexander.antonsen.arno@hotmail.com>
2026-08-19 20:54:17 +0200

    started search by rating

--- back-end/services/GameService.js
+++ back-end/services/GameService.js
@@ -21,7 +21,6 @@ class GameService {
 
       const accesstoken = response.data.access_token;
 
-
       const gameReq = await this.axios.post(
         "https://api.igdb.com/v4/games",
         "fields *;",
@@ -32,7 +31,6 @@ class GameService {
           },
         },
       );
-
 
       return gameReq.data;
     } catch (err) {
@@ -86,7 +84,6 @@ class GameService {
         },
       );
 
-
       return gameReq.data;
     } catch (err) {
       err.httpStatus = err.httpStatus || 500;
@@ -97,11 +94,7 @@ class GameService {
 
   // Search all endpoints
   async searchAllCata(payload) {
-
-    const {
-      search
-    } = payload;
-
+    const { search } = payload;
 
     const searchQ = (search || "").trim();
 
@@ -127,7 +120,6 @@ class GameService {
 
       const accesstoken = response.data.access_token;
 
-
       const gameReq = await this.axios.post(
         "https://api.igdb.com/v4/search",
         `fields *; search "${searchQ}";`,
@@ -139,7 +131,6 @@ class GameService {
         },
       );
 
-      
       return gameReq.data;
     } catch (err) {
       err.httpStatus = err.httpStatus || 500;
@@ -153,6 +144,56 @@ class GameService {
   // Search by genres
 
   // Order by rating
+
+  async searchByRating(rating) {
+    if (!rating) {
+      const err = new Error("Rating is required");
+      err.httpStatus = 400;
+      err.code = "APP_BAD_REQUEST";
+      throw err;
+    }
+
+    const ratingNorm = Number(rating);
+
+    if (!Number.isInteger(ratingNorm) || ratingNorm <= 0) {
+      const err = new Error("Rating must be a positive integer");
+      err.httpStatus = 400;
+      err.code = "APP_BAD_REQUEST";
+      throw err;
+    }
+    try {
+      const response = await this.axios.post(
+        "https://id.twitch.tv/oauth2/token?",
+        null,
+        {
+          params: {
+            client_id: process.env.IGDB_CLIENT_ID,
+            client_secret: process.env.IGDB_CLIENT_SECRET,
+            grant_type: "client_credentials",
+          },
+        },
+      );
+
+      const accesstoken = response.data.access_token;
+
+      const gameReq = await this.axios.post(
+        "https://api.igdb.com/v4/games",
+        "fields *;",
+        {
+          headers: {
+            "Client-ID": process.env.IGDB_CLIENT_ID,
+            Authorization: `Bearer ${accesstoken}`,
+          },
+        },
+      );
+
+      return gameReq.data;
+    } catch (err) {
+      err.httpStatus = err.httpStatus || 500;
+      err.code = err.code || "APP_INTERNAL";
+      throw err;
+    }
+  }
 }
 
 module.exports = GameService;
